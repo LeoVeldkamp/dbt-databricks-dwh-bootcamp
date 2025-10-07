dcf162f6c3847a062b09535197d6a1e99fab94bc
LeoVeldkamp <leo.veldkamp@cz.nl>
2025-10-07 11:41:34 +0200

    Auto commit for Studio handoff

--- models/weerdata_leo.sql
+++ models/weerdata_leo.sql
@@ -1,2 +1,21 @@
-select yyyymmdd as datum, tx as temperatuur_tiende, rh as regen_mm, sq as sunshine, pg as pressure_hpa
-from {{ source('weerdata', 'weerdata_gilze_rijen') }}
+WITH weerdata_gilze_rijen AS (
+  SELECT
+    *
+  FROM {{ source('weerdata', 'weerdata_gilze_rijen') }}
+), rename_1 AS (
+  SELECT
+    yyyymmdd AS datum,
+    tx AS temperatuur_tiende,
+    rh AS regen_mm,
+    sq AS sunshine,
+    pg AS pressure_hpa,
+    #STN AS hekje_stn
+  FROM weerdata_gilze_rijen
+), weerdata_leo AS (
+  SELECT
+    *
+  FROM rename_1
+)
+SELECT
+  *
+FROM weerdata_leo
