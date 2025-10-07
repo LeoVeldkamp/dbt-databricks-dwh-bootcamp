97c4f0afc5057c925adf25eb8a8f088a98fdb707
Leo Veldkamp <leo.veldkamp@cz.nl>
2025-10-07 08:27:04 +0000

    sunshine per hpa

--- models/weerdata_lineage.sql
+++ models/weerdata_lineage.sql
@@ -1,2 +1,3 @@
-select datum, temperatuur_tiende/10 as temperatuur_hele_graden, regen_mm 
+select datum, temperatuur_tiende/10 as temperatuur_hele_graden, regen_mm,
+sunshine/pressure_hpa as sunshine_per_hpa
 from {{ref('weerdata_leo')}}
